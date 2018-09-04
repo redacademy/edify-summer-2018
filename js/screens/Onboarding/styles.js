0983a5204935f658b349692da8baabaeee3d9c20
Andrei Egorov <andreyegorov1992@gmail.com>
2018-09-04 16:49:26 -0700

    Add Onboarding Screen and Style it

---
+++ js/screens/Onboarding/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  title: {
+    color: 'white',
+    fontSize: 24,
+    paddingBottom: 25,
+    flex: 0.2,
+  },
+  subTitle: {
+    color: 'white',
+    fontSize: 16,
+    flex: 1,
+  },
+  skipButton: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    paddingLeft: 30,
+  },
+  skip: {
+    color: 'white',
+    fontSize: 20,
+    textDecorationLine: 'underline',
+  },
+});
+
+export default styles;
