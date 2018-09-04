81584ac620d54a63e01e6eeceb38237583087289
Cody Rathor <codyrathor@gmail.com>
2018-09-04 15:41:57 -0700

    created dummy screens for Profile, Account, and Edify stacks

---
+++ js/screens/Account/AccountContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Account from './Account';
+
+export default class AccountContainer extends Component {
+  render() {
+    return <Account />;
+  }
+}
---
+++ js/screens/Edify/EdifyContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Edify from './Edify';
+
+export default class EdifyContainer extends Component {
+  render() {
+    return <Edify />;
+  }
+}
