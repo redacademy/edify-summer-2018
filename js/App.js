381c93bcb37ddca35790dc428cfe2a7f70003f55
Cody Rathor <codyrathor@gmail.com>
2018-09-04 13:55:08 -0700

    standardized prettier settings for the project

--- js/App.js
+++ js/App.js
@@ -5,7 +5,7 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
+    'Shake or press menu button for dev menu',
 });
 
 
@@ -27,16 +27,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
