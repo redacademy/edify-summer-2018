613e2ff3a5bf225d09fa4042a95b02b54c10bf5a
Cody Rathor <codyrathor@gmail.com>
2018-09-05 14:15:59 -0700

    added bottom tab navigator to project with icons

---
+++ js/navigation/NavigationLayout.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
+import AccountScreen from '../screens/Account';
+import ProfileScreen from '../screens/Profile';
+import EdifyScreen from '../screens/Edify';
+import { HumanIcon } from '../assets/HumanIcon';
+import { AccountIcon } from '../assets/AccountIcon';
+import { EdifyIcon } from '../assets/EdifyIcon';
+import { sharedNavigationOptions } from './config';
+
+const EdifyStack = createStackNavigator(
+  {
+    Edify: EdifyScreen,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation),
+    }),
+  },
+);
+const AccountStack = createStackNavigator(
+  {
+    Account: AccountScreen,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation),
+    }),
+  },
+);
+const ProfileStack = createStackNavigator(
+  {
+    Profile: ProfileScreen,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation),
+    }),
+  },
+);
+
+export default createBottomTabNavigator(
+  {
+    Profile: ProfileStack,
+    Account: AccountStack,
+    Edify: EdifyStack,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let icon;
+        switch (routeName) {
+          case 'Account':
+            icon = <AccountIcon color={tintColor} />;
+            break;
+          case 'Profile':
+            icon = <HumanIcon color={tintColor} />;
+            break;
+          case 'Edify':
+            icon = <EdifyIcon color={tintColor} />;
+            break;
+          default:
+            icon = <EdifyIcon color={tintColor} />;
+            break;
+        }
+        return icon;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#35377B',
+      inactiveTintColor: '#FFFFFF',
+      showLabel: false,
+      style: {
+        backgroundColor: '#75D2D1',
+        borderTopColor: '#75D2D1',
+        height: 64,
+      },
+    },
+  },
+);
