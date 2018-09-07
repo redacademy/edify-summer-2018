f56c772ea75751e3591fda927bd96591191264a0
Pamela Chong <pjchong91@gmail.com>
2018-09-07 13:08:50 -0700

    Refactored code to export resource component through index file

--- js/screens/Resources/Resources.js
+++ js/screens/Resources/Resources.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import styles from './styles';
-import Resource from './../../components/Resource/Resource';
+import Resource from './../../components/Resource';
 
 const Resources = ({ resources, navigation }) => {
   return (
