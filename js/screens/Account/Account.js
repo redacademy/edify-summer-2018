7cda5b3579a1281711e6decd3b3ba9048a869267
Effat Chaar <effat.shaar@gmail.com>
2018-09-07 15:03:17 -0700

    styled account page

--- js/screens/Account/Account.js
+++ js/screens/Account/Account.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
 
 const Account = props => {
   return (
-    <View>
-      <Text>Account Screen</Text>
+    <View style={styles.container}>
+
+      <View style={styles.firstView}>
+        <TouchableOpacity activeOpacity= {0.1}>
+          <Text style={styles.optionsText}>Change Password</Text>
+        </TouchableOpacity>
+
+        <View style={styles.divider}></View>
+
+        <TouchableOpacity activeOpacity= {0.1}>
+          <Text style={styles.optionsText}>Account Privacy</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableOpacity activeOpacity= {0.1}
+          style={styles.signOutButton}>
+          <Text style={styles.signOutText}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
