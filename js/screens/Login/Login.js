e4622c450bbb3209d122ee04ba27090228fbdb6d
Pamela Chong <pjchong91@gmail.com>
2018-09-12 15:35:26 -0700

    Adding feedback and validation to login.

--- js/screens/Login/Login.js
+++ js/screens/Login/Login.js
@@ -1,10 +1,28 @@
 import React, { Fragment } from 'react';
 import styles from './styles.js';
-import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ActivityIndicator,
+} from 'react-native';
 import { Form, Field } from 'react-final-form';
 
-//TODO: Validation and error feedback will be done later.  Persisting user required for data loading
-const Login = ({ navigation, signIn, user }) => {
+validate = values => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'Required';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  }
+  return errors;
+};
+const required = value => (value ? undefined : 'Required Field');
+
+const Login = ({ navigation, signIn, user, loginLoading, loginError }) => {
   return (
     <View style={styles.root}>
       <View style={styles.container}>
@@ -24,66 +42,96 @@ const Login = ({ navigation, signIn, user }) => {
         </View>
         <View style={styles.formContainer}>
           <Form
-            onSubmit={values =>
-              signIn({
-                variables: { email: values.email, password: values.password },
-              }).then(
-                res => {
-                  user.addUser(
-                    res.data.authenticateUser.id,
-                    res.data.authenticateUser.token,
-                  );
+            onSubmit={async values => {
+              try {
+                const res = await signIn({
+                  variables: { email: values.email, password: values.password },
+                });
+                user.addUser(
+                  res.data.authenticateUser.id,
+                  res.data.authenticateUser.token,
+                );
 
-                  res.data.authenticateUser.showOnboarding
-                    ? navigation.navigate('onBoarding', {
-                        userId: res.data.authenticateUser.id,
-                      })
-                    : navigation.navigate('Profile', {
-                        userId: res.data.authenticateUser.id,
-                      });
-                },
-                err => console.log(err),
-              )
-            }
-            // validate={validate}
+                res.data.authenticateUser.showOnboarding
+                  ? navigation.navigate('onBoarding', {
+                      userId: res.data.authenticateUser.id,
+                    })
+                  : navigation.navigate('Profile', {
+                      userId: res.data.authenticateUser.id,
+                    });
+              } catch (err) {
+                throw Error(err);
+              }
+            }}
+            validate={this.validate}
             render={({ handleSubmit, pristine, invalid }) => (
               <Fragment>
                 <View style={styles.inputContainer}>
-                  <Field
-                    name="email"
-                    render={({ input, meta }) => (
-                      <TextInput
-                        textContentType="emailAddress"
-                        placeholder="Email"
-                        {...input}
-                        style={styles.loginField}
-                      />
+                  <Field name="email" validate={required}>
+                    {({ input, meta }) => (
+                      <View style={styles.fieldContainer}>
+                        <TextInput
+                          textContentType="emailAddress"
+                          placeholder="Email"
+                          {...input}
+                          style={styles.loginField}
+                          autoCapitalize="none"
+                        />
 
-                      // ... {meta.touched && meta.error && <Text>{meta.error}</Text>}
+                        {meta.touched &&
+                          meta.error && (
+                            <Text style={styles.fieldError}>{meta.error}</Text>
+                          )}
+                      </View>
                     )}
-                  />
-                  <Field
-                    name="password"
-                    render={({ input, meta }) => (
-                      <TextInput
-                        textContentType="password"
-                        secureTextEntry={true}
-                        placeholder="Password"
-                        {...input}
-                        style={styles.loginField}
-                      />
+                  </Field>
+                  <Field name="password" validate={required}>
+                    {({ input, meta }) => (
+                      <View style={styles.fieldContainer}>
+                        <TextInput
+                          textContentType="password"
+                          secureTextEntry={true}
+                          placeholder="Password"
+                          {...input}
+                          style={styles.loginField}
+                        />
 
-                      // ... {meta.touched && meta.error && <Text>{meta.error}</Text>}
+                        {meta.touched &&
+                          meta.error && (
+                            <Text style={styles.fieldError}>{meta.error}</Text>
+                          )}
+                      </View>
                     )}
-                  />
+                  </Field>
                 </View>
+                <View style={styles.buttonAndHelp}>
+                  {loginError && (
+                    <Text style={styles.errorText}>
+                      Incorrect username or password
+                    </Text>
+                  )}
 
-                <TouchableOpacity
-                  style={styles.loginButtonContainer}
-                  onPress={handleSubmit}
-                >
-                  <Text style={styles.loginButton}>Log In</Text>
-                </TouchableOpacity>
+                  {loginLoading ? (
+                    <View style={styles.loginButtonContainer}>
+                      <ActivityIndicator
+                        style={styles.loginIndicator}
+                        size="small"
+                        color="white"
+                      />
+                    </View>
+                  ) : (
+                    <TouchableOpacity
+                      style={[
+                        styles.loginButtonContainer,
+                        invalid ? styles.inactiveLogin : null,
+                      ]}
+                      onPress={handleSubmit}
+                      disabled={invalid ? true : false}
+                    >
+                      <Text style={styles.loginButton}>Log In</Text>
+                    </TouchableOpacity>
+                  )}
+                </View>
               </Fragment>
             )}
           />
