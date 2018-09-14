61c66e4c7c8090d61cbf38e4a9837f135996519a
Andrei Egorov <andreyegorov1992@gmail.com>
2018-09-14 10:58:56 -0700

    1 Create ChildPost Screen and Add it to the navigation stack

--- js/screens/ChildPost/ChildPost.js
+++ js/screens/ChildPost/ChildPost.js
@@ -17,15 +17,16 @@ const ChildPost = ({ post, navigation }) => {
   return (
     <ScrollView>
       <View style={styles.root}>
-      
         <Image source={{ uri: post.imageurl }} style={styles.mainImage} />
         <View style={styles.textContainer}>
           <View style={styles.postMeta}>
             <Text style={styles.sideText}>Posted by </Text>
-            <TouchableOpacity style={styles.description}
-            onPress={() => {
-            navigation.navigate("Educator", { id: post.creator.id});
-          }}>
+            <TouchableOpacity
+              style={styles.description}
+              onPress={() => {
+                navigation.navigate('Educator', { id: post.creator.id });
+              }}
+            >
               <Text style={styles.creator}>{post.creator.firstname} </Text>
             </TouchableOpacity>
             <Text style={styles.sideText}>
@@ -50,30 +51,24 @@ const ChildPost = ({ post, navigation }) => {
             </View>
           </View>
 
-
-
-            <View style={styles.header}>
-              <Text style={styles.title}>Comments</Text>
-              <TouchableOpacity
-                activeOpacity={0.5}
-                onPress={() => close(true)}
-              >
-                <Text style={styles.close}>X</Text>
-              </TouchableOpacity>
-            </View>
-            <TextInput
-              {...this.props}
-              multiline={true}
-              // onChangeText={text => this.setState({ text })}
-              autoFocus
-              // value={this.state.text}
-              style={styles.textInput}
-            />
-            <TouchableOpacity activeOpacity={0.5} style={styles.button}>
-              <Text style={styles.submit}>Submit</Text>
+          <View style={styles.header}>
+            <Text style={styles.title}>Comments</Text>
+            <TouchableOpacity activeOpacity={0.5} onPress={() => close(true)}>
+              <Text style={styles.close}>X</Text>
             </TouchableOpacity>
           </View>
-     
+          <TextInput
+            {...this.props}
+            multiline={true}
+            // onChangeText={text => this.setState({ text })}
+            autoFocus
+            // value={this.state.text}
+            style={styles.textInput}
+          />
+          <TouchableOpacity activeOpacity={0.5} style={styles.button}>
+            <Text style={styles.submit}>Submit</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
