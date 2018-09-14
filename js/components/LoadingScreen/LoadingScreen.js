0fddf8fb4ec66e51e79c3b82d37f41cb2129f4ac
Andrei Egorov <andreyegorov1992@gmail.com>
2018-09-14 11:05:55 -0700

    Edit styling and remove unnecessary file

--- js/components/LoadingScreen/LoadingScreen.js
+++ js/components/LoadingScreen/LoadingScreen.js
@@ -5,7 +5,6 @@ import styles from './styles';
 export default (loadingScreen = () => {
   return (
     <Image
-      style={{ height: 100, width: 100 }}
       source={require('../../assets/gif/frogloading.gif')}
       style={styles.loadingScreen}
     />
